13b76fe054da0cbedd624523fe2c7ab697b47a93
Andrey <andreypomortsev@gmail.com>
2024-07-19 17:27:32 +0500

    Рефакторинг настройки тестов для использования setup_function для инициализации PATH_TO_TEST_FILE
    - Перенес инициализацию PATH_TO_TEST_FILE в setup_function.
    - Это улучшает читаемость и поддерживаемость тестов, устанавливая тестовую среду перед запуском каждой тестовой функции.
    - Удалена глобальная инициализация PATH_TO_TEST_FILE и перенесена внутрь setup_function.

--- tests/2.1/test_a.py
+++ tests/2.1/test_a.py
@@ -4,15 +4,22 @@ from io import StringIO
 from tests import solution_wrapper
 
 
-current_file_name = os.path.basename(__file__)
-current_dir = os.path.basename(os.path.dirname(__file__))
+PATH_TO_TEST_FILE = None
 
-# Получаем имя тестируемого файла
-tested_file_name = current_file_name.split("_")[-1]
-PATH_TO_TEST_FILE = os.path.join(current_dir, tested_file_name)
 
-# Оборачиваем тестируемый файл в функцию main
-solution_wrapper.wrapper(PATH_TO_TEST_FILE)
+def setup_function():
+    global PATH_TO_TEST_FILE
+
+    current_file_name = os.path.basename(__file__)
+    current_dir = os.path.basename(os.path.dirname(__file__))
+
+    # Получаем имя тестируемого файла
+    tested_file_name = current_file_name.split("_")[-1]
+    PATH_TO_TEST_FILE = os.path.join(current_dir, tested_file_name)
+
+    # Оборачиваем тестируемый файл в функцию main
+    solution_wrapper.wrapper(PATH_TO_TEST_FILE)
+
 
 from tests import wrapped_a
 
@@ -37,6 +44,7 @@ def test_code_statement():
             "print('Привет, Яндекс!')\n",
         }
         assert line in expected_output
+
 
 # Удаляем временный файл
 # os.remove(os.path.join("tests", "wrapped_a.py"))
