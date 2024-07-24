a3e82e8f33361a08bcb71f28590c96308c0c3592
Andrey <andreypomortsev@gmail.com>
2024-07-24 21:26:29 +0500

    #5 Код рефакторинг

--- solutions/2.1/f.py
+++ solutions/2.1/f.py
@@ -4,8 +4,10 @@ weight = int(input())
 bill = int(input())
 total = int(price * weight)
 change = bill - total
-print(f"""Чек
+print(
+    f"""Чек
 {name} - {weight}кг - {price}руб/кг
 Итого: {total}руб
 Внесено: {bill}руб
-Сдача: {change}руб""")
+Сдача: {change}руб"""
+)
