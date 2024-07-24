c3492ee06ba6656ae0c7475a3cee81470b788755
Andrey <andreypomortsev@gmail.com>
2024-07-24 21:25:15 +0500

    Добавил решение F 2.1

---
+++ solutions/2.1/f.py
@@ -0,0 +1,11 @@
+name = input()
+price = int(input())
+weight = int(input())
+bill = int(input())
+total = int(price * weight)
+change = bill - total
+print(f"""Чек
+{name} - {weight}кг - {price}руб/кг
+Итого: {total}руб
+Внесено: {bill}руб
+Сдача: {change}руб""")
