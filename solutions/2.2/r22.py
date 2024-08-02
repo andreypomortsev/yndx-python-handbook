80f5af77ab8c5f922aca271eff95b58015db1e9d
Andrey <andreypomortsev@gmail.com>
2024-08-02 19:34:02 +0500

    #24 Переписал решение из-за падения math.acos при больших int

--- solutions/2.2/r22.py
+++ solutions/2.2/r22.py
@@ -1,35 +1,20 @@
-import math
-
-
 side_a = float(input())
 side_b = float(input())
 side_c = float(input())
 
-# Вычисляем косинус каждого угла, используя Закон Косинусов
-cos_angle_a = (side_b**2 + side_c**2 - side_a**2) / (2 * side_b * side_c)
-cos_angle_b = (side_c**2 + side_a**2 - side_b**2) / (2 * side_c * side_a)
-cos_angle_c = (side_a**2 + side_b**2 - side_c**2) / (2 * side_a * side_b)
+if side_c < side_a:
+    side_c, side_a = side_a, side_c
+if side_c < side_b:
+    side_c, side_b = side_b, side_c
 
-# Вычисляем углы в радианах
-angle_a_in_radians = math.acos(cos_angle_a)
-angle_b_in_radians = math.acos(cos_angle_b)
-angle_c_in_radians = math.acos(cos_angle_c)
+sum_of_a_b_squares = side_a**2 + side_b**2
+c_square = side_c**2
 
-# Преобразуем углы в градусы
-angle_a = angle_a_in_radians * 180 / math.pi
-angle_b = angle_b_in_radians * 180 / math.pi
-angle_c = angle_c_in_radians * 180 / math.pi
+epsilon = 1e-9
 
-if angle_b < angle_a > angle_c:
-    max_angle = angle_a
-elif angle_a < angle_b > angle_c:
-    max_angle = angle_b
-else:
-    max_angle = angle_c
-
-if max_angle == 90:
+if abs(c_square - sum_of_a_b_squares) < epsilon:
     print("100%")
-elif max_angle < 90:
+elif c_square < sum_of_a_b_squares:
     print("крайне мала")
 else:
     print("велика")
