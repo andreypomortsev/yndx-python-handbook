6e07dfbab03264ddecba26234382a54d0c8cdda4
Andrey <andreypomortsev@gmail.com>
2024-08-02 16:49:03 +0500

    #26 Добавил тесты для решений задач P,Q,R,S,T 2.2

---
+++ tests/2.2/test_o22.py
@@ -0,0 +1,30 @@
+import pytest
+
+from io import StringIO
+from tests.data.test_data_22 import o_test_data
+
+MEMORY_LIMIT = 64  # RAM в MB
+TIME_LIMIT = 1  # Временной лимит в сек
+
+
+@pytest.mark.memory_limit(MEMORY_LIMIT)
+@pytest.mark.time_limit(TIME_LIMIT)
+@pytest.mark.parametrize(
+    "mock_input_text, expected_output, test_name",
+    o_test_data,
+    ids=[i[2] for i in o_test_data],  # Считываем названия тестов
+)
+def test_input_output(
+    monkeypatch, setup_environment, mock_input_text, expected_output, test_name
+):
+    wrapped_module, _ = setup_environment
+
+    mock_input = StringIO(mock_input_text)
+    mock_print = StringIO()
+    monkeypatch.setattr("sys.stdin", mock_input)
+    monkeypatch.setattr("sys.stdout", mock_print)
+
+    wrapped_module.main()
+
+    printed_output = mock_print.getvalue()
+    assert printed_output == expected_output
