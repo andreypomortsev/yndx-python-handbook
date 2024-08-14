255d24a82673e243f7bd0cdbf55e2d15cd020b72
Andrey <andreypomortsev@gmail.com>
2024-08-14 14:16:05 +0500

    #65 Добавил файлы с тестами решений задач A,B,C,D,E 3.2

---
+++ tests/3.2/test_32_d.py
@@ -0,0 +1,27 @@
+import pytest
+
+from tests.data.test_data_32 import d_test_data
+from tests.utils import assert_equal
+
+MEMORY_LIMIT = 64  # RAM в MB
+TIME_LIMIT = 1  # Временной лимит в сек
+
+
+@pytest.mark.memory_limit(MEMORY_LIMIT)
+@pytest.mark.time_limit(TIME_LIMIT)
+@pytest.mark.parametrize(
+    "mock_input_text, expected_output, test_name",
+    d_test_data,
+    ids=[i[2] for i in d_test_data],  # Считываем названия тестов
+)
+def test_input_output(
+    monkeypatch, setup_environment, mock_input_text, expected_output, test_name
+):
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        mock_input_text,
+        expected_output,
+        isinstance(expected_output, set),
+    )
