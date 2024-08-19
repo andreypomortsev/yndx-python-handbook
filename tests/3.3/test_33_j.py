529050a08ef9b96586f8fce633867714148a5294
Andrey <andreypomortsev@gmail.com>
2024-08-19 17:22:13 +0500

    #73 Написал тесты для всех задач в 3.3

--- tests/3.3/test_33_j.py
+++ tests/3.3/test_33_j.py
@@ -1,10 +1,8 @@
-from types import ModuleType
-from typing import Tuple
+from types import CodeType
 
 import pytest
 
 from tests.data.test_data_33 import j_test_data
-from tests.utils import assert_equal
 
 MEMORY_LIMIT = 64  # RAM в MB
 TIME_LIMIT = 1  # Временной лимит в сек
@@ -13,21 +11,19 @@ TIME_LIMIT = 1  # Временной лимит в сек
 @pytest.mark.memory_limit(MEMORY_LIMIT)
 @pytest.mark.time_limit(TIME_LIMIT)
 @pytest.mark.parametrize(
-    "mock_input_text, expected_output, test_name",
+    "variables, expected_output, _",  # _ - название теста
     j_test_data,
     ids=[i[2] for i in j_test_data],  # Считываем названия тестов
 )
-def test_input_output(
-    monkeypatch: pytest.MonkeyPatch,
-    setup_environment: Tuple[ModuleType, str],
-    mock_input_text: str,
+def test_comprehensions(
+    setup_environment_comprehension: CodeType,
+    variables: dict,
     expected_output: str,
-    test_name: str,
+    _: str,
 ) -> None:
-    wrapped_module, _ = setup_environment
-    assert_equal(
-        wrapped_module,
-        monkeypatch,
-        mock_input_text,
-        expected_output,
-    )
+    # Считываем код для проверки и компилируем его из строки в код
+    to_execute = setup_environment_comprehension
+
+    printed_output = eval(to_execute, variables)
+
+    assert printed_output == expected_output
