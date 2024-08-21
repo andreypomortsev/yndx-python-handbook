91ceb8c83dd9ac801916d67c406e6cfdaedb1ea6
Andrey <andreypomortsev@gmail.com>
2024-08-21 14:56:29 +0500

    #75 Отсанитайзил код перед использованием eval

--- tests/3.3/test_33_i.py
+++ tests/3.3/test_33_i.py
@@ -23,7 +23,9 @@ def test_comprehensions(
 ) -> None:
     # Считываем код для проверки и компилируем его из строки в код
     to_execute = setup_environment_comprehension
-
-    printed_output = eval(to_execute, variables)
+    safe_globals = {
+        "__builtins__": {"map": map, "str": str, "set": set, "sorted": sorted}
+    }
+    printed_output = eval(to_execute, safe_globals, variables)
 
     assert printed_output == expected_output
