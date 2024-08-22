1a583127fecf954b2ba45b46a9a551be80555f94
Andrey <andreypomortsev@gmail.com>
2024-08-22 17:26:36 +0500

    #78 Добавл файлы для тестов решений F,G,H,I,J 3.4

---
+++ tests/3.4/test_34_j.py
@@ -0,0 +1,33 @@
+from types import ModuleType
+from typing import Tuple
+
+import pytest
+
+from tests.data.test_data_34 import j_test_data
+from tests.utils import assert_equal
+
+MEMORY_LIMIT = 64  # RAM в MB
+TIME_LIMIT = 1  # Временной лимит в сек
+
+
+@pytest.mark.memory_limit(MEMORY_LIMIT)
+@pytest.mark.time_limit(TIME_LIMIT)
+@pytest.mark.parametrize(
+    "mock_input_text, expected_output, _",  # _ - название теста
+    j_test_data,
+    ids=[i[2] for i in j_test_data],  # Считываем названия тестов
+)
+def test_input_output(
+    monkeypatch: pytest.MonkeyPatch,
+    setup_environment: Tuple[ModuleType, str],
+    mock_input_text: str,
+    expected_output: str,
+    _: str,
+) -> None:
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        mock_input_text,
+        expected_output,
+    )
