e009fd2ceb5be675e55e2780e2646371d5890b5a
Andrey <andreypomortsev@gmail.com>
2024-08-26 19:59:55 +0500

    #86 Изменил ссылки на тестовые данные для задач C,D 3.5 на тестовые данные для задач I,K 3.1

--- tests/3.5/test_35_d.py
+++ tests/3.5/test_35_d.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 import pytest
 
-from tests.data.test_data_35 import d_test_data
+from tests.data.test_data_31 import k_test_data
 from tests.utils import assert_equal
 
 MEMORY_LIMIT = 64  # RAM в MB
@@ -14,8 +14,8 @@ TIME_LIMIT = 1  # Временной лимит в сек
 @pytest.mark.time_limit(TIME_LIMIT)
 @pytest.mark.parametrize(
     "mock_input_text, expected_output, _",  # _ - название теста
-    d_test_data,
-    ids=[i[2] for i in d_test_data],  # Считываем названия тестов
+    k_test_data,
+    ids=[i[2] for i in k_test_data],  # Считываем названия тестов
 )
 def test_input_output(
     monkeypatch: pytest.MonkeyPatch,
