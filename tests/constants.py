024ff9828eba424665e6ea308e955d99be94da5c
Andrey <andreypomortsev@gmail.com>
2024-09-01 20:56:43 +0500

    #88 Добавил постоянное имя файла для тестов решения задачи O 3.5

--- tests/constants.py
+++ tests/constants.py
@@ -2,5 +2,5 @@ MEMORY_LIMIT = 64  # RAM в MB
 TIME_LIMIT = 1  # Временной лимит в сек
 
 TEST_FILE_NAMES = {
-    "3.5": {"f": ["cyrillic.txt", "transliteration.txt"]},
+    "3.5": {"f": ["cyrillic.txt", "transliteration.txt"], "o": "scoring.json"},
 }
