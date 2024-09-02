6309d0a0ead54b5a7dfc598e390738759bbda3bf
Andrey <andreypomortsev@gmail.com>
2024-09-02 16:37:30 +0500

    #89 Добавил файлы с шаблонами тестов для решений P,Q,R,S,T 3.5

---
+++ tests/3.5/test_35_p.py
@@ -0,0 +1,31 @@
+from types import ModuleType
+from typing import Tuple
+
+import pytest
+
+from tests.constants import MEMORY_LIMIT, TIME_LIMIT
+from tests.data.test_data_35 import p_test_data
+from tests.utils import assert_equal
+
+
+@pytest.mark.memory_limit(MEMORY_LIMIT)
+@pytest.mark.time_limit(TIME_LIMIT)
+@pytest.mark.parametrize(
+    "mock_input_text, expected_output, _",  # _ - название теста
+    p_test_data,
+    ids=[i[2] for i in p_test_data],  # Считываем названия тестов
+)
+def test_input_output(
+    monkeypatch: pytest.MonkeyPatch,
+    setup_environment: Tuple[ModuleType, str],
+    mock_input_text: str,
+    expected_output: str,
+    _: str,
+) -> None:
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        mock_input_text,
+        expected_output,
+    )
