73eaa4a917966aa71067143341137ba536d40745
Andrey <andreypomortsev@gmail.com>
2024-09-06 12:25:17 +0500

    #95 Добавил файлы шаблоны тестов для решений задач 4.1

---
+++ tests/4.1/test_41_j.py
@@ -0,0 +1,28 @@
+from types import ModuleType
+from typing import Tuple
+
+import pytest
+
+from tests.data.test_data_41 import j_test_data
+from tests.utils import assert_equal
+
+
+@pytest.mark.parametrize(
+    "mock_input_text, expected_output, _",
+    j_test_data,
+    ids=[i[-1] for i in j_test_data],
+)
+def test_input_output(
+    monkeypatch: pytest.MonkeyPatch,
+    setup_environment: Tuple[ModuleType, str],
+    mock_input_text: str,
+    expected_output: str,
+    _: str,
+) -> None:
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        mock_input_text,
+        expected_output,
+    )
