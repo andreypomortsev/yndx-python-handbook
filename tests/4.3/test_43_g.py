1ab75d78c57e177c19e90181db54d4fd26a01e67
Andrey <andreypomortsev@gmail.com>
2024-09-19 11:43:24 +0500

    #107 Добавил файлы с шаблонами тестов для решений F,G,H,I,J 4.3

---
+++ tests/4.3/test_43_g.py
@@ -0,0 +1,21 @@
+from typing import Callable, Tuple
+
+import pytest
+
+from tests.data.test_data_43 import g_test_data
+
+
+@pytest.mark.parametrize(
+    "args, expected_output, _",
+    g_test_data,
+    ids=[i[-1] for i in g_test_data],
+)
+def test_input_output(
+    decorated_function: Callable,
+    args: Tuple[Tuple[int]],
+    expected_output: Tuple[int],
+    _: str,
+) -> None:
+    returned_output = decorated_function(*args)
+
+    assert returned_output == expected_output
