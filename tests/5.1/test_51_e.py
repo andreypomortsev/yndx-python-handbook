9a4046e7cafb39b26a900f645b4787d1cf98587d
Andrey <andreypomortsev@gmail.com>
2024-09-25 14:18:42 +0700

    #110 Добавил файлы с шаблонами тестов для решений A,B,C,D,E 5.1

---
+++ tests/5.1/test_51_e.py
@@ -0,0 +1,21 @@
+from typing import Callable, Tuple
+
+import pytest
+
+from tests.data.test_data_51 import e_test_data
+
+
+@pytest.mark.parametrize(
+    "args, expected_output, _",
+    e_test_data,
+    ids=[i[-1] for i in e_test_data],
+)
+def test_input_output(
+    decorated_function: Callable,
+    args: Tuple[Tuple[int]],
+    expected_output: Tuple[int],
+    _: str,
+) -> None:
+    returned_output = decorated_function(*args)
+
+    assert returned_output == expected_output
