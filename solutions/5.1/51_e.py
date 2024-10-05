766a975deb68da023aae53e3587d1edf62735602
Andrey <andreypomortsev@gmail.com>
2024-10-05 17:16:50 +0700

    #114 Change of the annotation style

--- solutions/5.1/51_e.py
+++ solutions/5.1/51_e.py
@@ -1,18 +1,17 @@
 class Rectangle:
-    def __init__(
-        self,
-        point_one: tuple[float | int, float | int],
-        point_two: tuple[float | int, float | int],
-    ):
+    def __init__(self, point_one: tuple, point_two: tuple) -> None:
         self.x_one, self.y_one = point_one
         self.x_two, self.y_two = point_two
+
         self.height = abs(self.y_one - self.y_two)
         self.width = abs(self.x_one - self.x_two)
 
-    def perimeter(self) -> float | int:
-        calculated_perimeter = (self.height + self.width) * 2
-        return round(calculated_perimeter, 2)
+    def perimeter(self) -> float:
+        calc_perimeter = (self.height + self.width) * 2
 
-    def area(self) -> float | int:
-        calculated_area = self.height * self.width
-        return round(calculated_area, 2)
+        return round(calc_perimeter, 2)
+
+    def area(self) -> float:
+        calc_area = self.height * self.width
+
+        return round(calc_area, 2)
--- solutions/5.1/51_f.py
+++ solutions/5.1/51_f.py
@@ -7,32 +7,37 @@ class Rectangle:
         self.width = abs(self.x_one - self.x_two)
 
     def perimeter(self) -> float:
-        return round((self.height + self.width) * 2, 2)
+        calc_perimeter = (self.height + self.width) * 2
+
+        return round(calc_perimeter, 2)
 
     def area(self) -> float:
-        return round(self.height * self.width, 2)
+        calc_area = self.height * self.width
+
+        return round(calc_area, 2)
 
     def get_pos(self) -> tuple:
         x = min(self.x_one, self.x_two)
         y = max(self.y_one, self.y_two)
+
         return round(x, 2), round(y, 2)
 
     def get_size(self) -> tuple:
         return round(self.width, 2), round(self.height, 2)
 
-    def move(self, dx: float, dy: float) -> None:
+    def move(self, dx: float | int, dy: float | int) -> None:
         self.x_one += dx
         self.x_two += dx
         self.y_one += dy
         self.y_two += dy
 
-    def resize(self, width: float, height: float) -> None:
+    def resize(self, width: float | int, height: float | int) -> None:
         self.width = width
         self.height = height
 
         x, y = self.get_pos()
 
         self.x_one = x
-        self.x_two = x + self.width
         self.y_one = y
+        self.x_two = x + self.width
         self.y_two = y - self.height
