2b7b10e28c57959e2db11ef0e166e941babdd48f
Andrey <andreypomortsev@gmail.com>
2024-10-05 21:12:55 +0700

    #114 Added solutions for A,B,C problems 5.2

---
+++ solutions/5.2/52_a.py
@@ -0,0 +1,23 @@
+class Point:
+    def __init__(self, x: float | int, y: float | int) -> None:
+        self.x = x
+        self.y = y
+
+    def move(self, x: float | int, y: float | int) -> None:
+        self.x += x
+        self.y += y
+
+    def length(self, other: "Point") -> float | int:
+        distance_squared = (other.x - self.x) ** 2 + (other.y - self.y) ** 2
+        return round(distance_squared**0.5, 2)
+
+
+class PatchedPoint(Point):
+    def __init__(self, *coordinates) -> None:
+        if not coordinates:
+            x, y = 0, 0
+        elif isinstance(coordinates[0], tuple):
+            x, y = coordinates[0]
+        else:
+            x, y = coordinates
+        super().__init__(x, y)
