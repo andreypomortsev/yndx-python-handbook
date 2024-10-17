02ff30f1ef93a684a0d61fafc495b5c0105ef4fd
Andrey <andreypomortsev@gmail.com>
2024-10-17 14:32:22 +0700

    Added time and memory decorators to the testing to control memory and time of solutions execution

--- tests/5.3/test_53_e.py
+++ tests/5.3/test_53_e.py
@@ -4,11 +4,12 @@ from typing import Callable, List, Tuple
 import pytest
 
 from tests import utils
+from tests.conftest import MEMORY_LIMIT, TIME_LIMIT
 from tests.data.test_data_53 import e_test_data
 
 
 @pytest.mark.parametrize(
-    "iterables, error, _",
+    "iterables, expected_error, _",
     e_test_data["Errors"],
     ids=[i[-1] for i in e_test_data["Errors"]],
 )
@@ -16,16 +17,16 @@ def test_merge_error(
     load_module: Callable[[str], ModuleType],
     request: pytest.FixtureRequest,
     iterables: Tuple[int | float, int | float],
-    error: Exception,
+    expected_error: Exception,
     _: str,
 ) -> None:
     file_path, _ = utils.get_tested_file_details(request)
     solution_module = load_module(file_path)
 
-    with pytest.raises(error) as info:
+    with pytest.raises(expected_error) as info:
         solution_module.merge(*iterables)
 
-    assert error.__name__ == info.type.__name__
+    assert expected_error.__name__ == info.type.__name__
 
 
 @pytest.mark.parametrize(
@@ -41,8 +42,11 @@ def test_merge(
     _: str,
 ) -> None:
     file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
+    solution = load_module(file_path)
 
-    returned = solution_module.merge(*numbers)
+    decorated_func = utils.memory_limit(MEMORY_LIMIT)(solution.merge)
+    decorated_func = utils.time_limit(TIME_LIMIT)(decorated_func)
+
+    returned = decorated_func(*numbers)
 
     assert returned == list(expected)
