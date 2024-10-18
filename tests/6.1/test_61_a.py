961e60822d3ecfc9dc336486273d51d4b09274ba
Andrey <andreypomortsev@gmail.com>
2024-10-18 20:03:00 +0700

    Added tests for the solutions for the problems A-E 6.1

--- tests/6.1/test_61_a.py
+++ tests/6.1/test_61_a.py
@@ -1,50 +1,28 @@
 from types import ModuleType
-from typing import Callable, Tuple
+from typing import Tuple
 
 import pytest
 
-from tests import utils
 from tests.data.test_data_61 import a_test_data
+from tests.utils import assert_equal
 
 
 @pytest.mark.parametrize(
-    "coefficients, expected_error, _",
-    a_test_data["Errors"],
-    ids=[i[-1] for i in a_test_data["Errors"]],
+    "real_number, expected_output, _",
+    a_test_data,
+    ids=[i[-1] for i in a_test_data],
 )
-def test_find_roots_error(
-    load_module: Callable[[str], ModuleType],
-    request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_error: str,
+def test_calculate_expression(
+    monkeypatch: pytest.MonkeyPatch,
+    setup_environment: Tuple[ModuleType, str],
+    real_number: int | float,
+    expected_output: float,
     _: str,
 ) -> None:
-    file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
-
-    with pytest.raises(Exception) as exc_info:
-        solution_module.find_roots(*coefficients)
-
-    assert expected_error == exc_info.type.__name__
-
-
-@pytest.mark.parametrize(
-    "coefficients, expected_return, _",
-    a_test_data["Values"],
-    ids=[i[-1] for i in a_test_data["Values"]],
-)
-def test_find_roots(
-    load_module: Callable[[str], ModuleType],
-    request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_return: Tuple[float, float],
-    _: str,
-) -> None:
-    file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
-
-    returned = solution_module.find_roots(*coefficients)
-
-    for returned_root, expexted_root in zip(returned, expected_return):
-        assert round(returned_root, 2) == expexted_root
-
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        str(real_number),
+        str(expected_output) + "\n",
+    )
--- tests/6.1/test_61_d.py
+++ tests/6.1/test_61_d.py
@@ -1,50 +1,28 @@
 from types import ModuleType
-from typing import Callable, Tuple
+from typing import Tuple
 
 import pytest
 
-from tests import utils
 from tests.data.test_data_61 import d_test_data
+from tests.utils import assert_equal
 
 
 @pytest.mark.parametrize(
-    "coefficients, expected_error, _",
-    d_test_data["Errors"],
-    ids=[i[-1] for i in d_test_data["Errors"]],
+    "line_of_nums, expected_output, _",
+    d_test_data,
+    ids=[i[-1] for i in d_test_data],
 )
-def test_find_roots_error(
-    load_module: Callable[[str], ModuleType],
-    request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_error: str,
+def test_geometric_mean(
+    monkeypatch: pytest.MonkeyPatch,
+    setup_environment: Tuple[ModuleType, str],
+    line_of_nums: str,
+    expected_output: str,
     _: str,
 ) -> None:
-    file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
-
-    with pytest.raises(Exception) as exc_info:
-        solution_module.find_roots(*coefficients)
-
-    assert expected_error == exc_info.type.__name__
-
-
-@pytest.mark.parametrize(
-    "coefficients, expected_return, _",
-    d_test_data["Values"],
-    ids=[i[-1] for i in d_test_data["Values"]],
-)
-def test_find_roots(
-    load_module: Callable[[str], ModuleType],
-    request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_return: Tuple[float, float],
-    _: str,
-) -> None:
-    file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
-
-    returned = solution_module.find_roots(*coefficients)
-
-    for returned_root, expexted_root in zip(returned, expected_return):
-        assert round(returned_root, 2) == expexted_root
-
+    wrapped_module, _ = setup_environment
+    assert_equal(
+        wrapped_module,
+        monkeypatch,
+        line_of_nums,
+        expected_output,
+    )
