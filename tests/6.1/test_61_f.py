17055816e46f58e9309a9b2e9e4cc39d9834d75e
Andrey <andreypomortsev@gmail.com>
2024-10-21 19:29:05 +0700

    Added numpy to poetry

--- tests/6.1/test_61_f.py
+++ tests/6.1/test_61_f.py
@@ -1,50 +1,34 @@
 from types import ModuleType
-from typing import Callable, Tuple
+from typing import Callable
 
+import numpy as np
 import pytest
 
 from tests import utils
+from tests.constants import MEMORY_LIMIT, TIME_LIMIT
 from tests.data.test_data_61 import f_test_data
 
 
 @pytest.mark.parametrize(
-    "coefficients, expected_error, _",
-    f_test_data["Errors"],
-    ids=[i[-1] for i in f_test_data["Errors"]],
+    "size, expected_matrix, _",
+    f_test_data,
+    ids=[i[-1] for i in f_test_data],
 )
-def test_find_roots_error(
+def test_multiplication_matrix(
     load_module: Callable[[str], ModuleType],
     request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_error: str,
+    size: int,
+    expected_matrix: np.array,
     _: str,
 ) -> None:
     file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
+    solution = load_module(file_path)
 
-    with pytest.raises(Exception) as exc_info:
-        solution_module.find_roots(*coefficients)
+    decorated_func = utils.memory_limit(MEMORY_LIMIT)(
+        solution.multiplication_matrix
+    )
+    decorated_func = utils.time_limit(TIME_LIMIT)(decorated_func)
 
-    assert expected_error == exc_info.type.__name__
+    returned_matrix = decorated_func(size)
 
-
-@pytest.mark.parametrize(
-    "coefficients, expected_return, _",
-    f_test_data["Values"],
-    ids=[i[-1] for i in f_test_data["Values"]],
-)
-def test_find_roots(
-    load_module: Callable[[str], ModuleType],
-    request: pytest.FixtureRequest,
-    coefficients: Tuple[int | float, int | float, int | float],
-    expected_return: Tuple[float, float],
-    _: str,
-) -> None:
-    file_path, _ = utils.get_tested_file_details(request)
-    solution_module = load_module(file_path)
-
-    returned = solution_module.find_roots(*coefficients)
-
-    for returned_root, expexted_root in zip(returned, expected_return):
-        assert round(returned_root, 2) == expexted_root
-
+    assert np.array_equal(returned_matrix, expected_matrix)
