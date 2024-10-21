dc832f352af5890fcd96f26a6c47bb7e7b10bc79
Andrey <andreypomortsev@gmail.com>
2024-10-21 12:04:49 +0700

    Changed the solution's logic

--- solutions/2.1/21_h.py
+++ solutions/2.1/21_h.py
@@ -1,3 +1,3 @@
 repeat, phrase = int(input()), input()
 phrase_to_print = f'Я больше никогда не буду писать "{phrase}"!\n' * repeat
-print(phrase_to_print.rstrip("\n"))
+print(phrase_to_print, end="")
