434cfdf8fdd5112348d00b21f7443baf93580a67
Andrey <andreypomortsev@gmail.com>
2024-10-31 21:24:57 +0700

    Refactoring Fix #136

--- solutions/2.4/24_a.py
+++ solutions/2.4/24_a.py
@@ -1,5 +1,6 @@
 n = int(input())
+ENDS = " \n"
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        print(i * j, end=" \n"[j == n])
+        print(i * j, end=ENDS[j == n])
--- solutions/2.4/24_c.py
+++ solutions/2.4/24_c.py
@@ -1,8 +1,8 @@
 n = int(input())
-
+ENDS = " \n"
 line = 1
 
 for i in range(1, n + 1):
     cond = i == (line * (line + 1)) // 2
     line += cond
-    print(i, end=" \n"[cond])
+    print(i, end=ENDS[cond])
