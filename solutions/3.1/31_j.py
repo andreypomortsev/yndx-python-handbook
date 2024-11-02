a4965ecd4800e12daf96243bdc444ba45039af68
Andrey <andreypomortsev@gmail.com>
2024-11-02 19:37:52 +0700

    Refactoror the solution, replaced join with a nested loop, removed spaces.

--- solutions/3.1/31_j.py
+++ solutions/3.1/31_j.py
@@ -2,22 +2,19 @@ CHAR_RANGE = 1104
 lines = []
 
 while (line := input()) != "ФИНИШ":
-    lines.append(line.lower())
+    lines.extend(line.lower().split())
 
-long_line = "".join(lines)
 char_count = [0] * CHAR_RANGE
 
-for char in long_line:
-    char_count[ord(char)] += 1
+for word in lines:
+    for char in word:
+        char_count[ord(char)] += 1
 
-most_common_char = ""
 max_count = 0
 
 for i in range(CHAR_RANGE):
-    if i == 32:  # chr(32) = " "
-        continue
     if char_count[i] > max_count:
         max_count = char_count[i]
         most_common_char = chr(i)
 
-print(most_common_char)
+print(most_common_char if most_common_char else "")
