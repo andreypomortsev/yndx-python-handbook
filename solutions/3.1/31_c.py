4cd57e1d74186e59daa0ece40878765e20f7547e
Andrey <andreypomortsev@gmail.com>
2024-11-02 19:34:52 +0700

    Removed -3 and moved +3 to indexing

--- solutions/3.1/31_c.py
+++ solutions/3.1/31_c.py
@@ -1,9 +1,9 @@
-max_length = int(input()) - 3
+max_length = int(input())
 n = int(input())
 
 for _ in range(n):
     sentence = input()
-    if len(sentence) > max_length + 3:
-        print(f"{sentence[:max_length]}...")
+    if len(sentence) > max_length:
+        print(f"{sentence[:max_length - 3]}...")
     else:
         print(sentence)
