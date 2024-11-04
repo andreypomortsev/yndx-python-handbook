93da5731432d8a7b8127fe2253c1d4c6661a2408
Andrey <andreypomortsev@gmail.com>
2024-11-04 18:27:27 +0700

    Refactoring: changed a method to count 1 in a string to str.count() which is way faster

--- solutions/3.2/32_o.py
+++ solutions/3.2/32_o.py
@@ -1,11 +1,11 @@
-digits = input().split()
+digits = map(int, input().split())
 
 answer = []
 
 for digit in digits:
-    binary = bin(int(digit))[2:]
+    binary = bin(digit)[2:]
     digits = len(binary)
-    units = sum(i == "1" for i in binary)
+    units = binary.count("1")
     zeros = digits - units
     answer.append({"digits": digits, "units": units, "zeros": zeros})
 
