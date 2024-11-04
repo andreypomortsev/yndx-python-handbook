658147162d75b13650c5068e18811b423cfb29a6
Andrey <andreypomortsev@gmail.com>
2024-11-04 18:40:14 +0700

    Refactoring: fixed error with 1, changed how the answer is printed Fix: #140

--- solutions/3.2/32_t.py
+++ solutions/3.2/32_t.py
@@ -1,16 +1,16 @@
-numbers = sorted(set(map(int, input().split("; "))))
+numbers = sorted(set(map(int, input().split(";"))))
 n = len(numbers)
 
 for i in range(n):
     co_primes = []
     for j in range(n):
-        if i != j:
-            a, b = numbers[i], numbers[j]
+        a, b = numbers[i], numbers[j]
+        if a > 1 < b and i != j:
             while b:
                 a, b = b, a % b
             if a == 1:
-                co_primes.append(numbers[j])
+                co_primes.append(str(numbers[j]))
 
     if co_primes:
-        print(f"{numbers[i]} - ", end="")
-        print(*co_primes, sep=", ")
+        answer = f"{numbers[i]} - {", ".join(co_primes)}"
+        print(answer)
