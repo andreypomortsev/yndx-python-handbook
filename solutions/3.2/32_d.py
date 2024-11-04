4a46b5d14206a793e4444421934a9527da44a0fc
Andrey <andreypomortsev@gmail.com>
2024-11-04 18:22:56 +0700

    Refactoring: rewrote intersection as &

--- solutions/3.2/32_d.py
+++ solutions/3.2/32_d.py
@@ -3,7 +3,7 @@ n, m = int(input()), int(input())
 porridge_one = {input() for _ in range(n)}
 porridge_two = {input() for _ in range(m)}
 
-length = len(porridge_one.intersection(porridge_two))
+length = len(porridge_one & porridge_two)
 
 if length:
     print(length)
