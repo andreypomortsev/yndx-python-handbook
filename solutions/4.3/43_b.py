b45193b4338f95acf708c08197278db6bd5d58f4
Andrey <andreypomortsev@gmail.com>
2024-11-08 11:20:56 +0700

    Refactoring: changed the logic from working with number as a string to numerical operations

--- solutions/4.3/43_b.py
+++ solutions/4.3/43_b.py
@@ -1,5 +1,4 @@
 def recursive_digit_sum(number: int) -> int:
-    number = str(number)
-    if len(number) == 1:
-        return int(number[0])
-    return int(number[0]) + recursive_digit_sum(number[1:])
+    if number < 10:
+        return number
+    return (number % 10) + recursive_digit_sum(number // 10)
