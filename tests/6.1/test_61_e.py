a611f457e361badb9f839a191a4c88605e39079f
Andrey <andreypomortsev@gmail.com>
2024-11-11 12:46:18 +0700

    Changed the type of comparing floats

--- tests/6.1/test_61_e.py
+++ tests/6.1/test_61_e.py
@@ -1,10 +1,11 @@
+import math
+from io import StringIO
 from types import ModuleType
 from typing import Tuple
 
 import pytest
 
 from tests.data.test_data_61 import e_test_data
-from tests.utils import assert_equal
 
 
 @pytest.mark.parametrize(
@@ -16,13 +17,18 @@ def test_find_distance(
     monkeypatch: pytest.MonkeyPatch,
     setup_environment: Tuple[ModuleType, str],
     coordinates: str,
-    expected_output: str,
+    expected_output: float,
     _: str,
 ) -> None:
     wrapped_module, _ = setup_environment
-    assert_equal(
-        wrapped_module,
-        monkeypatch,
-        coordinates,
-        expected_output,
-    )
+
+    mock_input = StringIO(coordinates)
+    mock_print = StringIO()
+    monkeypatch.setattr("sys.stdin", mock_input)
+    monkeypatch.setattr("sys.stdout", mock_print)
+
+    wrapped_module.main()
+
+    printed_output = float(mock_print.getvalue().rstrip())
+
+    assert math.isclose(printed_output, expected_output, rel_tol=1e-6)
