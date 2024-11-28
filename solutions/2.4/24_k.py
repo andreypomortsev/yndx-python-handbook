0d0227f15c81d2eb4721547d08862875f9e85d45
Andrey <andreypomortsev@gmail.com>
2024-11-28 13:06:50 +0700

    optimise the prime calculations

--- solutions/2.4/24_k.py
+++ solutions/2.4/24_k.py
@@ -4,12 +4,22 @@ prime_nums_counter = 0
 
 for _ in range(n):
     number = int(input())
-    if number < 2:
+
+    if number <= 1:
         continue
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            break
-    else:
+    elif number <= 3:
         prime_nums_counter += 1
+    elif number % 2 == 0 or number % 3 == 0:
+        continue
+    else:
+        i = 5
+        sqrt = int(number**0.5) + 1
+        while i <= sqrt:
+            if number % i == 0 or number % (i + 2) == 0:
+                is_prime = False
+                break
+            i += 6
+        else:
+            prime_nums_counter += 1
 
 print(prime_nums_counter)
