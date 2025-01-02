3b96e13b72e91705dea86434283051d35379e4ae
Andrey <andreypomortsev@gmail.com>
2025-01-02 18:40:58 +0700

    Re-wrote solution, now it reads a file from the tail

--- solutions/3.5/35_j.py
+++ solutions/3.5/35_j.py
@@ -1,12 +1,33 @@
-DEFAULT_ENCODING = {"encoding": "UTF-8"}
+DEFAULT_ENCODING = "UTF-8"
+SIZE = 32
 
 file_name = input()
-tail = int(input())
+tail = int(input()) + 1
 
-lines = []
+with open(file_name, "rb") as file:
+    file.seek(0, 2)
+    pointer_location = file.tell()
+    buffer = bytearray()
 
-with open(file_name, "r", **DEFAULT_ENCODING) as file:
-    while line := file.readline():
-        lines.append(line)
+    read_lines = 0
+    lines = []
 
-print(*lines[-tail:], sep="")
+    while pointer_location > 0 and read_lines <= tail + 1:
+        chunk_size = min(pointer_location, SIZE)
+        file.seek(pointer_location - chunk_size)
+
+        chunk = file.read(chunk_size)
+        pointer_location -= chunk_size
+
+        buffer = chunk + buffer
+        lines = buffer.split(b"\n")
+
+        read_lines = len(lines)
+
+        if read_lines >= tail + 1:
+            break
+
+
+for line in lines[-tail:]:
+    line_to_print = line.decode(DEFAULT_ENCODING)
+    print(line_to_print)
