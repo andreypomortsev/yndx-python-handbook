36c676ea8c6a4d36e08414e2865b87b03770165f
Andrey <andreypomortsev@gmail.com>
2025-01-04 08:51:00 +0700

    Refactoring: removed useless parts of the solution, improved redability and understanding.

--- solutions/3.5/35_t.py
+++ solutions/3.5/35_t.py
@@ -1,12 +1,8 @@
-CHUNK_SIZE = 4096
+BYTES = 2
 sum_of_numbers = 0
 
 with open("numbers.num", "rb") as file:
-    while chunk := file.read(CHUNK_SIZE):
-        for i in range(0, len(chunk) - 1, 2):
-            # fmt: off
-            two_bytes = chunk[i: i + 2]
-            # fmt: on
-            sum_of_numbers += int.from_bytes(two_bytes, byteorder="big")
+    while chunk := file.read(BYTES):
+        sum_of_numbers += int.from_bytes(chunk, byteorder="big")
 
-print(sum_of_numbers & 0xFFFF)
+print(sum_of_numbers % 256**BYTES)
