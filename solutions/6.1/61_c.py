15870b8ad4178e685bb109a08aa6b9a548ef8c17
Andrey <andreypomortsev@gmail.com>
2025-01-14 19:26:16 +0700

    Fixed misspelled variable name

--- solutions/6.1/61_c.py
+++ solutions/6.1/61_c.py
@@ -3,9 +3,9 @@ import math
 
 def get_chanses(n: int, m: int) -> tuple:
     all_options = math.comb(n, m)
-    vasya_in_opions = all_options * m // n
+    vasya_in_options = all_options * m // n
 
-    return vasya_in_opions, all_options
+    return vasya_in_options, all_options
 
 
 guests, seats = map(int, input().split())
