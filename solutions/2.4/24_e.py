fb51b75c5a49887d3cd45eaeb346aa1328173f5e
Andrey <andreypomortsev@gmail.com>
2025-06-04 17:49:49 +0700

    change naming

--- solutions/2.4/24_e.py
+++ solutions/2.4/24_e.py
@@ -3,8 +3,8 @@ total = 0
 
 for _ in range(n):
     bunnies = 0
-    while (words := input()) != "ВСЁ":
-        bunnies += words == "зайка"
+    while (word := input()) != "ВСЁ":
+        bunnies += word == "зайка"
     total += bool(bunnies)
 
 print(total)
