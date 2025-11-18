ea5eeb94ada514fa95ca900c5d467acaa9706dcf
Andrey <andreypomortsev@gmail.com>
2025-11-18 16:24:30 +0700

    update: refactor

--- tests/data/test_data_53.py
+++ tests/data/test_data_53.py
@@ -9,7 +9,7 @@ def get_value_error() -> None:
 
     Raises: ValueError
     """
-    x = int("Hello, world!")
+    int("Hello, world!")
 
 
 def get_type_error() -> None:
@@ -18,7 +18,7 @@ def get_type_error() -> None:
 
     Raises: TypeError
     """
-    x = "2" + 2
+    "2" + 2
 
 
 def get_system_error() -> None:
