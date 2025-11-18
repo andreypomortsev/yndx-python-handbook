271ff41b44d576447db848e324c96af2923a1427
Andrey <andreypomortsev@gmail.com>
2025-11-18 16:22:16 +0700

    update: refactor

--- solutions/4.1/41_e.py
+++ solutions/4.1/41_e.py
@@ -1,17 +1,16 @@
-__counter = 0
+_counter = 0
 
 
 def click() -> None:
     """
     Increments the global counter variable.
     """
-    global __counter
-    __counter += 1
+    global _counter
+    _counter += 1
 
 
 def get_count() -> int:
     """
     Returns the value of the global counter variable.
     """
-    global __counter
-    return __counter
+    return _counter
